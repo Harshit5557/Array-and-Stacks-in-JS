70560f8e6d2039fb6d8acf7c9ad40c33f448a100
Harshit5557 <akhilendra054@gmail.com>
2024-09-11 17:57:27 +0530

    Initial Commit

---
+++ Array.js
@@ -0,0 +1,9 @@
+var data=[
+    "Hyundai Tarjet",
+    "Nissan Friend",
+    "Ford Probe",
+    "Mazda Titan",
+];
+
+console.log(data);
+console.log(data[2]);
---
+++ Array_Meathods.js
@@ -0,0 +1,10 @@
+var data=[
+    "Hyundai Tarjet",
+    "Nissan Friend",
+    "Ford Probe",
+    "Mazda Titan",
+];
+
+console.log(data.join('*'));
+console.log(data.pop());
+console.log(data.join());
